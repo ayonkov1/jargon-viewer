7fff09543bf2ae7e221fbdb6d71af1ad0fcd2097
ayonkov1 <atanasyonkov1@gmail.com>
2023-01-26 09:20:37 +0200

    Initial commit three.js

---
+++ components/Canvas/Canvas.styled.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+
+export const StyledBox = styled(Box)({
+    maxWidth: 500,
+    maxHeight: 500
+})
---
+++ components/Canvas/Canvas.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import THREE from "three";
+import { StyledBox } from "./Canvas.styled"
+
+
+const Canvas = () => {
+
+
+
+    return (
+        <StyledBox>
+            Text
+        </StyledBox>
+    )
+}
+
+export default Canvas
